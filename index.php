a94f212bf3780582a800847c5483a3ac49937c52
matheusgoulart07 <matheus.goulart@unesp.br>
2025-03-11 19:36:08 -0300

    atualizacao

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+ $city = "Bauru"; $dia = 6; $mes = "Março"; $ano = 2025; # declarando as variáveis
+ while ($dia < 31){
+ echo "<b>$city</b>, $dia de $mes de $ano<br>"; #saída
+ $dia+=7;
+ }
+?>
